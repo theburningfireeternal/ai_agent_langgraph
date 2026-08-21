6bbc04ffb304f85d3694f8dcb2a82115e0e89a46
saurabh305 <saurabhchandra305@gmail.com>
2026-08-21 18:29:11 +0530

    react workflow using langgraph

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.messages import HumanMessage
+from langgraph.graph import MessagesState, StateGraph, END
+from nodes import run_agent_reasoning, tool_node
 
 load_dotenv()
 
+AGENT_REASON = "agent_reason"
+ACT="act"
+LAST = -1
 
-def main():
-    print("Hello from ai-agent-langraph!")
+def should_continue(state: MessagesState) -> str:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+flow = StateGraph(MessagesState)
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END: END,
+    ACT:ACT
+})
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph(). draw_mermaid_png(output_file_path="flow.png")
 
 
 if __name__ == "__main__":
-    print(os.getenv("OPENAI_API_KEY"))
-    main()
+    print("Hello ReAct Langgraph with function calling")
+    res = app.invoke({"messages" : [HumanMessage(content="What is the weather in new delhi? List it and then triple it")]})
+    print(res["messages"][LAST].content)
